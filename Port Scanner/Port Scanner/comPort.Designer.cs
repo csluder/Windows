04c0c7666dd2e7a8d576a6d6803fc582bb6e73b8
csluder <csluder@yahoo.com>
2016-04-28 15:01:20 -0600

    Added files via upload

---
+++ Port Scanner/Port Scanner/comPort.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class comPort
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Rescan = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // Rescan
+            // 
+            this.Rescan.Location = new System.Drawing.Point(197, 227);
+            this.Rescan.Name = "Rescan";
+            this.Rescan.Size = new System.Drawing.Size(75, 23);
+            this.Rescan.TabIndex = 0;
+            this.Rescan.Text = "Rescan";
+            this.Rescan.UseVisualStyleBackColor = true;
+            this.Rescan.Click += new System.EventHandler(this.Rescan_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(260, 199);
+            this.listBox1.TabIndex = 1;
+            // 
+            // comPort
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Rescan);
+            this.Name = "comPort";
+            this.Text = "Chuck\'s Port Scanner";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Rescan;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Port Scanner/Port Scanner/comPort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace WindowsFormsApplication1
+{
+    public partial class comPort : Form
+    {
+        public comPort()
+        {
+            InitializeComponent();	
+		
+            //Set list of serial ports for the list box
+            String[] portNames = SerialPort.GetPortNames();
+
+            if (portNames.Length == 0)
+            {
+                portNames = new String[] { "No Ports Detected" };
+            }
+            Array.Sort( portNames );
+            listBox1.Items.AddRange(portNames);
+        }
+
+        private void Rescan_Click(object sender, EventArgs e)
+        {
+            //Set list of serial ports for the list box
+            String[] portNames = SerialPort.GetPortNames();
+
+            if (portNames.Length == 0)
+            {
+                portNames = new String[] { "No Ports Detected" };
+            }
+            listBox1.Items.Clear();
+            Array.Sort(portNames);
+            listBox1.Items.AddRange(portNames);
+        }
+    }
+}
